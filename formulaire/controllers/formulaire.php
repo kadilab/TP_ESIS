b48f9a75e88b0e924fccb436d81c05e017f7b867
YvesKalume <19kn132@esisalama.org>
2022-01-21 10:04:06 +0200

    creation des formulaires

---
+++ formulaire/controllers/formulaire.php
@@ -0,0 +1,22 @@
+<?php
+
+// Verification des données envoyées
+if (isset($_POST['nom']) && $_POST['postnom'] && $_POST['matricule'] && $_POST['genre'] && $_POST['promotion'] && $_POST['email'] && $_POST['prenom']) {
+    // Recuperation des données
+    $name = $_POST['nom'];
+    $postnom = $_POST['postnom'];
+    $prenom = $_POST['prenom'];
+    $matricule = $_POST['matricule'];
+    $genre = $_POST['genre'];
+    $promotion = $_POST['promotion'];
+    $email = $_POST['email'];
+    // Affichqge des données envoyées sur une autre page
+    echo 'Nom ' . $name . ' ' . $postnom . ' ' . $prenom . '<br>';
+    echo 'Promotion ' . $promotion . '<br>';
+    echo 'Genre ' . $genre . '<br>';
+    echo 'Email ' . $email . '<br>';
+    echo 'Matricule ' . $matricule . '<br>';
+} else {
+    echo "Formulaire invalide";
+}
+
---
+++ formulaire/formulaire.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire</title>
+</head>
+<body>
+    <form action="controllers/formulaire.php" method="post">
+        Nom : <input type="text" name="nom"><br><br>
+        Post-nom : <input type="text" name="postnom"><br><br>
+        Prenom : <input type="text" name="prenom"><br><br>
+        Matricule : <input type="text" name="matricule"><br><br>
+        Genre : <select name="genre"><br><br>
+            <option value="M">M</option>
+            <option value="F">F</option>
+        </select><br><br>
+        Promotion : <input type="text" name="promotion"><br><br>
+        Email : <input type="email" name="email"><br><br>
+
+        <input type="submit">
+    </form>
+</body>
+</html>
